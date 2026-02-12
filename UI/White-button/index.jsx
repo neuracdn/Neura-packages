f15d22c716c3a6b0c4a49c2889023415d180f94d
neuracdn <ato247012@gmail.com>
2026-02-12 22:41:59 +0530

    chore: update UI/White-button/index.jsx via Neura AI

--- UI/White-button/index.jsx
+++ UI/White-button/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './index.css';
 
-const Index = ({ children, className = '', text = 'Click Me', ...props }) => {
+const Index = ({ children, className = '', text = 'Click Mwwe', ...props }) => {
   return (
     <div className={`main-container ${className}`} {...props}>
-      <button className='button'>{text}</button>
+      <button className="button">{text}</button>
       {children}
     </div>
   );
